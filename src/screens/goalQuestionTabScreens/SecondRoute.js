8c23bbe5e2aec8add6604d7cd228f07d8b9eb297
daminduLiyanage <damindu7@gmail.com>
2021-02-02 14:48:53 +0530

    modal recreated

--- src/screens/goalQuestionTabScreens/SecondRoute.js
+++ src/screens/goalQuestionTabScreens/SecondRoute.js
@@ -6,8 +6,10 @@ import {
   Text,
   TouchableOpacity,
   View,
-  Modal,
+  Dimensions, 
+  Modal
 } from 'react-native';
+// import Modal from 'react-native-modal';
 import layout from '../../theme/layout';
 import colors from '../../theme/colors';
 import CheckCircle from '../../assets/images/CheckCircle';
@@ -18,10 +20,12 @@ const SecondRoute = (onPress) => {
   const HEADING_GAP = 44;
   const HEADING_OFFSET = 120;
   const MODAL_RATIO = 302 / 384;
+  const DEVICE_WIDTH = Dimensions.get('screen').width;
+  const DEVICE_HEIGHT = Dimensions.get('screen').height; 
   const [modalVisible, setModalVisible] = useState(false);
   return (
-    <View style={{ flexGrow: 1 }}>
-      <View style={{ lor: 'red', flexGrow: 1, height: '100%', marginTop: SAFEVIEW_OFFSET, }}>
+    <View style={{ flexGrow: 1, borderWidth:1 }}>
+      <View style={{   flexGrow: 1, height: '100%', marginTop: SAFEVIEW_OFFSET, }}>
         <View style={{ flex: 23, paddingHorizontal: layout.padding.screenHorizontal, }}>
           <View style={{ flex: 14, justifyContent: 'center' }}>
             <Text style={{ flexBasis: HEADING_GAP, fontFamily: layout.fonts.nunito, color: colors.themeColors.primary, fontSize: layout.fontSizes.xxxLarge, }}> How? </Text>
@@ -42,134 +46,19 @@ const SecondRoute = (onPress) => {
           </View>
         </View>
       </View>
-      <Modal  transparent={true} visible={modalVisible}  >
+      <Modal  style={{   }} transparent={true} visible={modalVisible} onBackdropPress={() => setModalVisible(false)} >
         <View style={{ flex: 1, backgroundColor: colors.themeColors.transparent, alignItems: 'center', justifyContent: 'center' }}>
           <View style={{ flexBasis: 193 }}></View>
-          
+
           <View style={{ alignItems: 'center', aspectRatio: MODAL_RATIO, width: '80.8%', backgroundColor: colors.themeColors.primary, borderRadius: 30, shadowColor: '#000', shadowOffset: { height: 3, }, }} >
-            <Text style={{fontSize:20, fontFamily:layout.fonts.nunito, color:colors.themeColors.pink}}>Set a reminder</Text>
+            <Text style={{ fontSize: 20, fontFamily: layout.fonts.nunito, color: colors.themeColors.pink }}>Set a reminder</Text>
             <TouchableOpacity>
-             
+
             </TouchableOpacity>
           </View>
         </View>
       </Modal>
-    </View>
-
-
-
-
-
-
-    //   <View style={{borderWidth: 5, borderColor: 'green'}}>
-    //     <SafeAreaView
-    //       style={{
-    //         borderWidth: 1,
-    //         borderColor: 'red',
-    //         flexGrow: 1,
-    //         height: '100%',
-    //         marginTop: SAFEVIEW_OFFSET,
-    //       }}>
-    //       <View
-    //         style={{
-    //           flex: 23,
-    //           paddingHorizontal: layout.padding.screenHorizontal,
-    //         }}>
-    //         <View style={{flex: 14, justifyContent: 'center'}}>
-    //           <Text
-    //             style={{
-    //               flexBasis: HEADING_GAP,
-    //               fontFamily: layout.fonts.nunito,
-    //               color: colors.themeColors.primary,
-    //               fontSize: layout.fontSizes.xxxLarge,
-    //             }}>
-    //             How?
-    //           </Text>
-    //           <Text
-    //             style={{
-    //               fontFamily: layout.fonts.nunito,
-    //               color: colors.themeColors.pink,
-    //               fontSize: layout.fontSizes.welcomeText,
-    //             }}>
-    //             How will you achieve this goal?
-    //           </Text>
-    //           <View style={{flexBasis: HEADING_OFFSET}} />
-    //         </View>
-
-    //         <View style={{flex: 16, alignItems: 'center'}}>
-    //           <TaskItem
-    //             style={{position: 'absolute', top: 0}}
-    //             label="+Add Task/Habit"
-    //             backgroundColor={colors.themeColors.pink}
-    //             fontColor={colors.themeColors.primary}
-    //             onPress={() => {
-    //               setModalVisible(true);
-    //             }}
-    //           />
-
-    //           {/* <TaskItem
-    // label="Stop Eating Out"
-    // backgroundColor={colors.themeColors.primary}
-    // fontColor={colors.themeColors.secondary}
-    // /> */}
-    //         </View>
-    //       </View>
-
-    //       <View
-    //         style={{flex: 9, paddingHorizontal: layout.padding.screenHorizontal}}>
-    //         <View style={{flex: 26}}>
-    //           <TouchableOpacity
-    //             onPress={onPress}
-    //             style={{
-    //               flex: 1,
-    //               alignItems: 'center',
-    //               justifyContent: 'flex-start',
-    //             }}>
-    //             <CheckCircle />
-    //           </TouchableOpacity>
-    //         </View>
-    //       </View>
-    //     </SafeAreaView>
-    //     <Modal transparent={true} visible={modalVisible}>
-    //       <View
-    //         style={{
-    //           borderWidth: 1,
-    //           borderColor: 'red',
-    //           height: '100%',
-    //           justifyContent: 'center',
-    //           alignItems: 'center',
-    //           backgroundColor: colors.themeColors.transparentBlack,
-    //         }}>
-    //         <View style={{flexBasis: 310}} />
-    //         <View
-    //           style={{
-    //             width: '80%',
-    //             height: '50%',
-    //             backgroundColor: colors.themeColors.primary,
-    //             borderRadius: 20,
-    //             padding: 35,
-    //             alignItems: 'center',
-    //             shadowColor: '#000',
-    //             shadowOffset: {
-    //               width: 5,
-    //               height: 2,
-    //             },
-    //             shadowOpacity: 0.25,
-    //             shadowRadius: 3.84,
-    //             elevation: 5,
-    //           }}>
-    //           <Text style={styles.modalText}>Hello World!</Text>
-    //           <TouchableOpacity
-    //             style={{...styles.openButton, backgroundColor: '#2196F3'}}
-    //             onPress={() => {
-    //               setModalVisible(!modalVisible);
-    //             }}>
-    //             <Text style={styles.textStyle}>Hide Modal</Text>
-    //           </TouchableOpacity>
-    //         </View>
-    //       </View>
-    //     </Modal>
-    //   </View>
+    </View> 
   );
 };
 const styles = StyleSheet.create({
