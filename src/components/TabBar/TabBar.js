051774a9097f435de8bfe348e641208fad83c1ec
daminduLiyanage <damindu7@gmail.com>
2021-02-03 11:02:06 +0530

    Avaca logo restored

--- src/components/TabBar/TabBar.js
+++ src/components/TabBar/TabBar.js
@@ -5,7 +5,7 @@ import {SafeAreaView} from 'react-native-safe-area-context';
 import TabItem from './TabItem';
 import MyTasksIcon from '../SvgIcons/MyTasksIcon';
 import MyGoalsIcon from '../SvgIcons/MyGoalsIcon';
-const AvacadoIcon = require('../../assets/images/AvocadoLogo.png');
+const AvacaIcon = require('../../assets/images/Avaca.png');
 import layout from '../../theme/layout';
 import {TouchableOpacity} from 'react-native-gesture-handler';
 const TabBar = ({state, descriptors, navigation}) => {
@@ -26,7 +26,7 @@ const TabBar = ({state, descriptors, navigation}) => {
 
   const avacado = {
     name: 'Avoca.do',
-    icon: () => <Image source={AvacadoIcon} />,
+    icon: () => <Image source={AvacaIcon} />,
   };
 
   const TABS = [myTasks, myGoals, avacado];
