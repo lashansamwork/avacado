b03c0372b5cd620a214b3f99f7651e7db754d049
daminduLiyanage <damindu7@gmail.com>
2021-02-07 18:56:41 +0530

    backdrop press funtion added

--- src/screens/goalQuestionTabScreens/HowToAchieveGoalScreen.js
+++ src/screens/goalQuestionTabScreens/HowToAchieveGoalScreen.js
@@ -1,5 +1,11 @@
 import React, {useState} from 'react';
-import {Text, TouchableOpacity, View, Modal} from 'react-native';
+import {
+  Text,
+  TouchableOpacity,
+  TouchableWithoutFeedback,
+  View,
+  Modal,
+} from 'react-native';
 // import Modal from 'react-native-modal';
 import layout from '../../theme/layout';
 import colors from '../../theme/colors';
@@ -99,138 +105,153 @@ const HowToAchieveGoalScreen = (onPress) => {
             alignItems: 'center',
             justifyContent: 'center',
           }}>
-          <View style={{flexBasis: MODAL_OFFSET}} />
-
-          <View
+          <TouchableOpacity
             style={{
+              flex: 1,
+              width: '100%',
               alignItems: 'center',
-              aspectRatio: MODAL_RATIO,
-              width: MODAL_WIDTH,
-              backgroundColor: colors.themeColors.primary,
-              borderRadius: MODAL_RADIUS,
-              shadowColor: colors.themeColors.shadow,
-              shadowOffset: {height: MODAL_SHADOW_HEIGHT},
+              justifyContent: 'center',
+              borderWidth: 5,
+              borderColor: 'yellow',
+            }}
+            transparent={true}
+            onPressOut={() => {
+              setModalVisible(false);
             }}>
-            <View style={{flex: 7, justifyContent: 'flex-end'}}>
-              <Text
-                style={{
-                  fontSize: layout.fontSizes.welcomeText,
-                  fontFamily: layout.fonts.nunito,
-                  color: colors.themeColors.pink,
-                }}>
-                Set a reminder
-              </Text>
-            </View>
-            <View style={{flexBasis: DATE_PICKER_OFFSET}} />
-            <View
-              style={{
-                flex: 11,
-                overflow: 'hidden',
-                alignItems: 'center',
-                justifyContent: 'center',
-                width: PICKER_CONTAINER_WIDTH,
-              }}>
+            <View style={{flexBasis: MODAL_OFFSET}} />
+            <TouchableWithoutFeedback touchSoundDisabled>
               <View
+                activeOpacity={1}
                 style={{
-                  flex: 0.8,
-                  transform: [{scale: DATEPICKER_MAGNIFICATION}],
+                  borderWidth: 5,
+                  borderColor: 'red',
                   alignItems: 'center',
-                  justifyContent: 'center',
+                  aspectRatio: MODAL_RATIO,
+                  width: MODAL_WIDTH,
+                  backgroundColor: colors.themeColors.primary,
+                  borderRadius: MODAL_RADIUS,
+                  shadowColor: colors.themeColors.shadow,
+                  shadowOffset: {height: MODAL_SHADOW_HEIGHT},
                 }}>
-                <DatePicker
-                  date={date}
-                  onDateChange={setDate}
-                  mode={'time'}
-                  textColor={colors.themeColors.pink}
-                />
-              </View>
-            </View>
-            <View style={{flexBasis: DATE_PICKER_OFFSET}} />
-
-            <View
-              style={{
-                flex: 10,
-                alignItems: 'center',
-                justifyContent: 'space-evenly',
-                overflow: 'hidden',
-                width: PICKER_CONTAINER_WIDTH,
-              }}>
-              <View
-                style={{
-                  position: 'absolute',
-                  top: 0,
-                  width: SEPERATOR_WIDTH,
-                  height: layout.heights.seperatorHeight,
-                  backgroundColor: colors.themeColors.seperatorColor,
-                }}
-              />
-              <Text
-                style={{
-                  fontSize: layout.fontSizes.medium,
-                  color: colors.themeColors.pink,
-                  fontFamily: layout.fonts.nunito,
-                }}>
-                Repeat every
-              </Text>
-              <View style={{flex: 0.5}}>
-                <DayList />
-              </View>
-              <Text
-                style={{
-                  position: 'absolute',
-                  bottom: 0,
-                  width: SEPERATOR_WIDTH,
-                  height: layout.heights.seperatorHeight,
-                  backgroundColor: colors.themeColors.seperatorColor,
-                }}
-              />
-            </View>
-
-            <View
-              style={{
-                flex: 12,
-                alignItems: 'center',
-                justifyContent: 'flex-start',
-              }}>
-              <Text
-                style={{
-                  paddingTop: layout.padding.small,
-                  fontSize: layout.fontSizes.medium,
-                  color: colors.themeColors.pink,
-                  fontFamily: layout.fonts.nunito,
-                }}>
-                Times
-              </Text>
-
-              <View
-                style={{
-                  height: WHEELPICKER_HEIGHT,
-                  alignItems: 'center',
-                  justifyContent: 'center',
-                  overflow: 'hidden',
-                }}>
-                <WheelPicker
-                  selectedIndicatorStyle={{
-                    backgroundColor: colors.themeColors.primary,
-                    color: 'red',
-                  }}
-                  options={[1, 2, 3, 4, 5, 6, 7, 8, 9]}
-                  selected={times}
-                  onChange={(times) => setTimes(times)}
-                  itemHeight={WHEEL_ITEM_HEIGHT}
-                  containerStyle={{
-                    backgroundColor: colors.themeColors.primary,
+                <View style={{flex: 7, justifyContent: 'flex-end'}}>
+                  <Text
+                    style={{
+                      fontSize: layout.fontSizes.welcomeText,
+                      fontFamily: layout.fonts.nunito,
+                      color: colors.themeColors.pink,
+                    }}>
+                    Set a reminder
+                  </Text>
+                </View>
+                <View style={{flexBasis: DATE_PICKER_OFFSET}} />
+                <View
+                  style={{
+                    flex: 11,
                     overflow: 'hidden',
-                  }}
-                  itemTextStyle={{
-                    fontFamily: layout.fonts.nunito,
-                    color: colors.themeColors.pink,
-                  }}
-                  itemStyle={{backgroundColor: colors.themeColors.primary}}
-                />
+                    alignItems: 'center',
+                    justifyContent: 'center',
+                    width: PICKER_CONTAINER_WIDTH,
+                  }}>
+                  <View
+                    style={{
+                      flex: 0.8,
+                      transform: [{scale: DATEPICKER_MAGNIFICATION}],
+                      alignItems: 'center',
+                      justifyContent: 'center',
+                    }}>
+                    <DatePicker
+                      date={date}
+                      onDateChange={setDate}
+                      mode={'time'}
+                      textColor={colors.themeColors.pink}
+                    />
+                  </View>
+                </View>
+                <View style={{flexBasis: DATE_PICKER_OFFSET}} />
+                <View
+                  style={{
+                    flex: 10,
+                    alignItems: 'center',
+                    justifyContent: 'space-evenly',
+                    overflow: 'hidden',
+                    width: PICKER_CONTAINER_WIDTH,
+                  }}>
+                  <View
+                    style={{
+                      position: 'absolute',
+                      top: 0,
+                      width: SEPERATOR_WIDTH,
+                      height: layout.heights.seperatorHeight,
+                      backgroundColor: colors.themeColors.seperatorColor,
+                    }}
+                  />
+                  <Text
+                    style={{
+                      fontSize: layout.fontSizes.medium,
+                      color: colors.themeColors.pink,
+                      fontFamily: layout.fonts.nunito,
+                    }}>
+                    Repeat every
+                  </Text>
+                  <View style={{flex: 0.5}}>
+                    <DayList />
+                  </View>
+                  <Text
+                    style={{
+                      position: 'absolute',
+                      bottom: 0,
+                      width: SEPERATOR_WIDTH,
+                      height: layout.heights.seperatorHeight,
+                      backgroundColor: colors.themeColors.seperatorColor,
+                    }}
+                  />
+                </View>
+                <View
+                  style={{
+                    flex: 12,
+                    alignItems: 'center',
+                    justifyContent: 'flex-start',
+                  }}>
+                  <Text
+                    style={{
+                      paddingTop: layout.padding.small,
+                      fontSize: layout.fontSizes.medium,
+                      color: colors.themeColors.pink,
+                      fontFamily: layout.fonts.nunito,
+                    }}>
+                    Times
+                  </Text>
+                  <View
+                    style={{
+                      height: WHEELPICKER_HEIGHT,
+                      alignItems: 'center',
+                      justifyContent: 'center',
+                      overflow: 'hidden',
+                    }}>
+                    <WheelPicker
+                      selectedIndicatorStyle={{
+                        backgroundColor: colors.themeColors.primary,
+                        color: 'red',
+                      }}
+                      options={[1, 2, 3, 4, 5, 6, 7, 8, 9]}
+                      selected={times}
+                      onChange={(times) => setTimes(times)}
+                      itemHeight={WHEEL_ITEM_HEIGHT}
+                      containerStyle={{
+                        backgroundColor: colors.themeColors.primary,
+                        overflow: 'hidden',
+                      }}
+                      itemTextStyle={{
+                        fontFamily: layout.fonts.nunito,
+                        color: colors.themeColors.pink,
+                      }}
+                      itemStyle={{backgroundColor: colors.themeColors.primary}}
+                    />
+                  </View>
+                </View>
               </View>
-            </View>
-          </View>
+            </TouchableWithoutFeedback>
+          </TouchableOpacity>
         </View>
       </Modal>
     </View>
