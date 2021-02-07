cad281e1f37bf417b76715f211d66e1daebb3953
daminduLiyanage <damindu7@gmail.com>
2021-02-07 20:24:38 +0530

    model to components. task name input made

---
+++ src/components/Modals/TaskNameModal.js
@@ -0,0 +1,77 @@
+import React, {useState} from 'react';
+import {Text, View, TouchableOpacity} from 'react-native';
+import layout from '../../theme/layout';
+import colors from '../../theme/colors';
+import CustomTextInput from '../CustomTextInput';
+import CheckCircle from '../../assets/images/CheckCircle';
+
+const TaskNameModal = () => {
+  const MODAL_RATIO = 302 / 384;
+  const MODAL_WIDTH = '80.8%';
+  const MODAL_RADIUS = 30;
+  const MODAL_SHADOW_HEIGHT = 3;
+  const TEXT_INPUT_OFFSET = 30;
+  const PICKER_CONTAINER_WIDTH = '85%';
+  const TEXT_GAP = 2;
+  const [name, setName] = React.useState('');
+  return (
+    <View
+      activeOpacity={1}
+      style={{
+        alignItems: 'center',
+        aspectRatio: MODAL_RATIO,
+        width: MODAL_WIDTH,
+        backgroundColor: colors.themeColors.primary,
+        borderRadius: MODAL_RADIUS,
+        shadowColor: colors.themeColors.shadow,
+        shadowOffset: {height: MODAL_SHADOW_HEIGHT},
+      }}>
+      <View style={{flex: 7, justifyContent: 'flex-end'}}>
+        <Text
+          style={{
+            fontSize: layout.fontSizes.welcomeText,
+            fontFamily: layout.fonts.nunito,
+            color: colors.themeColors.pink,
+          }}>
+          What is the task name?
+        </Text>
+      </View>
+      <View style={{flexBasis: TEXT_INPUT_OFFSET}} />
+      <View
+        style={{
+          width: '60%',
+          flexShrink: 1,
+          borderBottomColor: colors.themeColors.pink,
+          borderBottomWidth: 1,
+          alignItems: 'center',
+        }}>
+        <CustomTextInput
+          textColor={colors.themeColors.secondary}
+          style={{
+            color: colors.themeColors.secondary,
+            borderBottomWidth: 10,
+            borderBottomColor: 'red',
+          }}
+          gap={TEXT_GAP}
+          placeholder="Enter the task name in here"
+          value={name}
+          onChangeText={(text) => setName(text)}
+        />
+      </View>
+      <View
+        style={{
+          flex: 11,
+          overflow: 'hidden',
+          alignItems: 'center',
+          justifyContent: 'center',
+          width: PICKER_CONTAINER_WIDTH,
+        }}>
+        <TouchableOpacity style={{alignSelf: 'center'}}>
+          <CheckCircle />
+        </TouchableOpacity>
+      </View>
+    </View>
+  );
+};
+
+export default TaskNameModal;
--- src/screens/goalQuestionTabScreens/HowToAchieveGoalScreen.js
+++ src/screens/goalQuestionTabScreens/HowToAchieveGoalScreen.js
@@ -11,30 +11,16 @@ import layout from '../../theme/layout';
 import colors from '../../theme/colors';
 import CheckCircle from '../../assets/images/CheckCircle';
 import TaskItem from '../../components/TaskItem';
-import DatePicker from 'react-native-date-picker';
-import WheelPicker from 'react-native-wheely';
-
-import DayList from '../../components/MultipleDaySelector/DayList';
+import TaskTimeModal from '../../components/Modals/TaskTimeModal';
+import TaskNameModal from '../../components/Modals/TaskNameModal';
 
 const HowToAchieveGoalScreen = (onPress) => {
   const SAFEVIEW_OFFSET = 15;
   const HEADING_GAP = 44;
   const HEADING_OFFSET = 120;
-  const MODAL_RATIO = 302 / 384;
   const MODAL_OFFSET = 200;
-  const MODAL_WIDTH = '80.8%';
-  const MODAL_RADIUS = 30;
-  const MODAL_SHADOW_HEIGHT = 3;
-  const DATE_PICKER_OFFSET = 30;
-  const PICKER_CONTAINER_WIDTH = '85%';
-  const DATEPICKER_MAGNIFICATION = 1.1;
-  const WHEEL_ITEM_HEIGHT = 18;
-  const WHEELPICKER_HEIGHT = '48%';
-  const SEPERATOR_WIDTH = '95%';
 
   const [modalVisible, setModalVisible] = useState(false);
-  const [date, setDate] = useState(new Date());
-  const [times, setTimes] = useState(5);
 
   return (
     <View style={{flexGrow: 1}}>
@@ -111,8 +97,6 @@ const HowToAchieveGoalScreen = (onPress) => {
               width: '100%',
               alignItems: 'center',
               justifyContent: 'center',
-              borderWidth: 5,
-              borderColor: 'yellow',
             }}
             transparent={true}
             onPressOut={() => {
@@ -120,135 +104,8 @@ const HowToAchieveGoalScreen = (onPress) => {
             }}>
             <View style={{flexBasis: MODAL_OFFSET}} />
             <TouchableWithoutFeedback touchSoundDisabled>
-              <View
-                activeOpacity={1}
-                style={{
-                  borderWidth: 5,
-                  borderColor: 'red',
-                  alignItems: 'center',
-                  aspectRatio: MODAL_RATIO,
-                  width: MODAL_WIDTH,
-                  backgroundColor: colors.themeColors.primary,
-                  borderRadius: MODAL_RADIUS,
-                  shadowColor: colors.themeColors.shadow,
-                  shadowOffset: {height: MODAL_SHADOW_HEIGHT},
-                }}>
-                <View style={{flex: 7, justifyContent: 'flex-end'}}>
-                  <Text
-                    style={{
-                      fontSize: layout.fontSizes.welcomeText,
-                      fontFamily: layout.fonts.nunito,
-                      color: colors.themeColors.pink,
-                    }}>
-                    Set a reminder
-                  </Text>
-                </View>
-                <View style={{flexBasis: DATE_PICKER_OFFSET}} />
-                <View
-                  style={{
-                    flex: 11,
-                    overflow: 'hidden',
-                    alignItems: 'center',
-                    justifyContent: 'center',
-                    width: PICKER_CONTAINER_WIDTH,
-                  }}>
-                  <View
-                    style={{
-                      flex: 0.8,
-                      transform: [{scale: DATEPICKER_MAGNIFICATION}],
-                      alignItems: 'center',
-                      justifyContent: 'center',
-                    }}>
-                    <DatePicker
-                      date={date}
-                      onDateChange={setDate}
-                      mode={'time'}
-                      textColor={colors.themeColors.pink}
-                    />
-                  </View>
-                </View>
-                <View style={{flexBasis: DATE_PICKER_OFFSET}} />
-                <View
-                  style={{
-                    flex: 10,
-                    alignItems: 'center',
-                    justifyContent: 'space-evenly',
-                    overflow: 'hidden',
-                    width: PICKER_CONTAINER_WIDTH,
-                  }}>
-                  <View
-                    style={{
-                      position: 'absolute',
-                      top: 0,
-                      width: SEPERATOR_WIDTH,
-                      height: layout.heights.seperatorHeight,
-                      backgroundColor: colors.themeColors.seperatorColor,
-                    }}
-                  />
-                  <Text
-                    style={{
-                      fontSize: layout.fontSizes.medium,
-                      color: colors.themeColors.pink,
-                      fontFamily: layout.fonts.nunito,
-                    }}>
-                    Repeat every
-                  </Text>
-                  <View style={{flex: 0.5}}>
-                    <DayList />
-                  </View>
-                  <Text
-                    style={{
-                      position: 'absolute',
-                      bottom: 0,
-                      width: SEPERATOR_WIDTH,
-                      height: layout.heights.seperatorHeight,
-                      backgroundColor: colors.themeColors.seperatorColor,
-                    }}
-                  />
-                </View>
-                <View
-                  style={{
-                    flex: 12,
-                    alignItems: 'center',
-                    justifyContent: 'flex-start',
-                  }}>
-                  <Text
-                    style={{
-                      paddingTop: layout.padding.small,
-                      fontSize: layout.fontSizes.medium,
-                      color: colors.themeColors.pink,
-                      fontFamily: layout.fonts.nunito,
-                    }}>
-                    Times
-                  </Text>
-                  <View
-                    style={{
-                      height: WHEELPICKER_HEIGHT,
-                      alignItems: 'center',
-                      justifyContent: 'center',
-                      overflow: 'hidden',
-                    }}>
-                    <WheelPicker
-                      selectedIndicatorStyle={{
-                        backgroundColor: colors.themeColors.primary,
-                        color: 'red',
-                      }}
-                      options={[1, 2, 3, 4, 5, 6, 7, 8, 9]}
-                      selected={times}
-                      onChange={(times) => setTimes(times)}
-                      itemHeight={WHEEL_ITEM_HEIGHT}
-                      containerStyle={{
-                        backgroundColor: colors.themeColors.primary,
-                        overflow: 'hidden',
-                      }}
-                      itemTextStyle={{
-                        fontFamily: layout.fonts.nunito,
-                        color: colors.themeColors.pink,
-                      }}
-                      itemStyle={{backgroundColor: colors.themeColors.primary}}
-                    />
-                  </View>
-                </View>
+              <View>
+                <TaskNameModal />
               </View>
             </TouchableWithoutFeedback>
           </TouchableOpacity>
