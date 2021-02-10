e707a6c0992cadc795612055147934552f204503
daminduLiyanage <damindu7@gmail.com>
2021-02-10 10:59:20 +0530

    style fixed

--- src/screens/GoalAddedScreen.js
+++ src/screens/GoalAddedScreen.js
@@ -12,6 +12,10 @@ import colors from '../theme/colors';
 const CongratzHand = require('../assets/images/CongratzHand.png');
 
 const GoalAddedScreen = () => {
+  const ASPECT_RATIO = 998 / 928;
+
+  let name = 'Layan';
+
   return (
     <SafeAreaView
       style={{
@@ -21,8 +25,8 @@ const GoalAddedScreen = () => {
       }}>
       <StatusBar barStyle="dark-content" />
       <TouchableOpacity style={{flex: 1, alignItems: 'center'}}>
-        <View style={{flex: 2}} />
-        <View style={{aspectRatio: 998 / 928, height: layout.heights.medium}}>
+        <View style={{flexBasis: 202}} />
+        <View style={{aspectRatio: ASPECT_RATIO, height: '25.1%'}}>
           <Image
             style={{flex: 1, width: null, height: null}}
             source={CongratzHand}
@@ -44,19 +48,20 @@ const GoalAddedScreen = () => {
               color: colors.themeColors.pink,
               fontSize: layout.fontSizes.xxxLarge,
             }}>
-            Congrats Layan!
+            Congrats {name}!
           </Text>
           <Text
             style={{
               flexShrink: 1,
               textAlign: 'center',
-              fontFamily: 'Nunito-SemiBold',
-              fontSize: layout.fontSizes.large,
+              fontFamily: layout.fonts.nunito,
+              fontSize: layout.fontSizes.header,
               color: colors.themeColors.primary,
             }}>
             You added a new goal that will change your life journey!
           </Text>
         </View>
+        <View style={{flexBasis: 128}} />
       </TouchableOpacity>
     </SafeAreaView>
   );
