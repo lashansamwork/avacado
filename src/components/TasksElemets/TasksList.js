e7cf261a1e45c88ffcfcf4a4c490e3023e93dfbd
daminduLiyanage <damindu7@gmail.com>
2021-03-09 14:13:08 +0530

    comments removed

--- src/components/TasksElemets/TasksList.js
+++ src/components/TasksElemets/TasksList.js
@@ -13,7 +13,6 @@ import colors from '../../theme/colors';
 import layout from '../../theme/layout';
 import TaskItem from '../TaskItem';
 import Tick from '../SvgIcons/TickIcon';
-//const Tick = require('../../assets/images/TickIcon.png');
 const TouchableHighlightAnimated = Animated.createAnimatedComponent(
   TouchableOpacity,
 );
